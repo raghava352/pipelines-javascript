c5edb20f2283b8bc0f6845f6e8cd97444b153ae0
raghava352 <52049546+raghava352@users.noreply.github.com>
2019-07-17 12:33:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample by Rakesh!\n');
+  res.send('Hello Node.js Sample by Rakesh after CD!\n');
 });
 
 var port = process.env.PORT||PORT;
